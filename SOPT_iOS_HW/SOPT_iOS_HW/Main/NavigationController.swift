cb69d4535b1d8b07ce78b3a72b77ec96c5f08276
L-j-h-c <ckrgkswnsgh@naver.com>
2021-10-17 03:24:53 +0900

    🔨[FIX] : 계정만들기 버튼 활성화 오류 해결 (#3)
    다른 계정으로 로그인하기 버튼 클릭 시 '계정만들기'가 활성화되지 않는 오류 해결.
    NavigationController로 타겟을 바꿔줬음.

---
+++ SOPT_iOS_HW/SOPT_iOS_HW/Main/NavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationController.swift
+//  SOPT_iOS_HW1
+//
+//  Created by Junho Lee on 2021/10/17.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
