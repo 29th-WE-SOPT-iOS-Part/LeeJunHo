2ca1d44faf60db69b4d95a5655df772a48fccde1
L-j-h-c <ckrgkswnsgh@naver.com>
2021-10-29 23:06:33 +0900

    ✅[CHORE] : 카테고리 컬렉션뷰 레이아웃 수정 (#7)
    컬렉션뷰 레이아웃 수정.

--- SOPT_iOS_HW/SOPT_iOS_HW/TabBar/HomeCategoryViewCell.swift
+++ SOPT_iOS_HW/SOPT_iOS_HW/TabBar/HomeCategoryViewCell.swift
@@ -14,17 +14,14 @@ class HomeCategoryViewCell: UICollectionViewCell {
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var categoryUIView: UIView!
     
-    
-    
     func setUIConfiguration(){
         categoryUIView.backgroundColor = .white
         categoryUIView.layer.backgroundColor = UIColor(red: 0.949, green: 0.949, blue: 0.949, alpha: 1).cgColor
-        categoryUIView.layer.cornerRadius = 12
+        categoryUIView.layer.cornerRadius = 17
         categoryUIView.layer.borderWidth = 1
         categoryUIView.layer.borderColor = UIColor(red: 0.854, green: 0.854, blue: 0.854, alpha: 1).cgColor
         
     }
-    
     
     override func awakeFromNib() {
         super.awakeFromNib()
